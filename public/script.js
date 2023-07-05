9ec9eae90a93e73910a4d68d0c99e51f1b81103c
Amy Mathews <amy.mathews0808@gmail.com>
2023-07-04 17:33:57 -0700

    finished the cover page

---
+++ public/script.js
@@ -0,0 +1,61 @@
+// this function allows the peeps to appear and show depending on the scroll
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show')
+      } else {
+          entry.target.classList.remove('show')
+      }
+    });
+  });
+  
+  const hiddenVals = document.querySelectorAll('.hidden');
+  hiddenVals.forEach((el) => observer.observe(el));
+
+  const STORAGE_KEY = "theme";
+const THEME_ATTR  = "data-theme";
+const QUERY_KEY   = "(prefers-color-scheme: dark)";
+
+const themes = {
+  LIGHT: "light",
+  DARK: "dark",
+};
+
+initTheme();
+
+function initTheme() {
+  const savedTheme = localStorage.getItem(STORAGE_KEY);
+
+  if (savedTheme) {
+    // Storage theme
+    setTheme(savedTheme);
+  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+    // system theme
+    setTheme(themes.DARK);
+  } else {
+    // Default theme
+    setTheme(themes.LIGHT);
+  }
+
+  // Watch for system theme changes
+  window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
+    const newTheme = e.matches ? themes.DARK : themes.LIGHT;
+    setTheme(newTheme);
+  });
+}
+
+function toggleTheme() {
+  const theme = getTheme();
+  const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
+  setTheme(newTheme);
+  localStorage.setItem(STORAGE_KEY, newTheme);
+}
+
+function getTheme() {
+  return document.documentElement.getAttribute(THEME_ATTR);
+}
+
+function setTheme(value) {
+  document.documentElement.setAttribute(THEME_ATTR, value);
+}
